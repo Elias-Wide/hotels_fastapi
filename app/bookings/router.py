0bbce54bb7e54ef06b60b3c1d2f264067fe4da9b
Ilya Shirokov <ilshi2904@yandex.ru>
2025-01-13 23:59:54 +0300

    test

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,19 +1,16 @@
 from fastapi import APIRouter
+from sqlalchemy import select
+
+from app.bookings.models import Bookings
+from app.database import async_session_maker
 
 
 router = APIRouter(prefix="/bookings", tags=["Бронирование"])
 
 
 @router.get("")
-def get_bookings():
-    pass
-
-
-@router.get("{/{booking_id}")
-def get_bookings(booking_id: int):
-    pass
-
-
-# @router.get('')
-# def get_bookings():
-#     pass
+async def get_bookings():
+    async with async_session_maker() as session:
+        query = select(Bookings)
+        result = await session.execute(query)
+        return result.scalars().all()
