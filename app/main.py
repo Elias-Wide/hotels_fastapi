3f93647c8a144febaadd89b125e72e3dec7dcb3b
Ilya Shirokov <ilshi2904@yandex.ru>
2025-01-24 00:03:31 +0300

    add css styles

--- app/main.py
+++ app/main.py
@@ -20,16 +20,19 @@ app.include_router(router_images)
 
 
 origins = [
-    'http://localhost:3000',
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH', 'PUT'],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
     allow_headers=[
-        'Content-Type', 'Set-Cookie', 'Access-Control-Allow-Headers',
-        'Access-Control-Allow-Origin', 'Authorization'
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
     ],
 )
